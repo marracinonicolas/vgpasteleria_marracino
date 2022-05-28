b5b5d7ebbb388b3af99ebb8f64b2e869f3e698ba
Nicolas Marracino <38430036+marracinonicolas@users.noreply.github.com>
2022-05-27 22:38:15 -0300

    paquete de fontawsome, icono en cart widget

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,6 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
 const CartWidget = () =>{
     return(
-        <span>Cart</span>
+        <a className='cartWidget'>
+            <FontAwesomeIcon icon={faShoppingCart} />
+            
+        </a>
     )
 }
 
