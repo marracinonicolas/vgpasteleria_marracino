43facfc83dbaf41f1ca990d7b9fa609ac99b3a24
Nicolas Marracino <38430036+marracinonicolas@users.noreply.github.com>
2022-05-27 22:51:49 -0300

    limpio codigo por defecto

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.scss';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
@@ -9,22 +8,6 @@ function App() {
         <div className="App">
             <NavBar />
             <ItemListContainer greeting="Hola Coders!" />
-            
-
-            {/* <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                Learn React
-                </a>
-            </header> */}
         </div>
     );
 }
